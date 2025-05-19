004df168600c84557a4ee99f89f53ff37e08200d
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2025-05-19 21:06:43 +0530

    will be updated later

---
+++ 1.LOGIC BUILDING/checkpower.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class checkpower {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter first number");
+        int x=ob.nextInt();
+        System.out.println("enter second number");
+        int y=ob.nextInt();
+        
+    }
+}
